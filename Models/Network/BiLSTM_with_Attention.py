a5e05d419879d339ea7fa495f18e55ea3a0783d7
Deepak Singh <negid0253@gmail.com>
2025-02-12 23:59:13 +0530

    fix: Resolved problem with Lambda by creating custom Attention Layer for neural network

--- Models/Network/BiLSTM_with_Attention.py
+++ Models/Network/BiLSTM_with_Attention.py
@@ -1,32 +1,89 @@
 import tensorflow as tf
-from tensorflow.keras.layers import Input, LSTM, Bidirectional, Dense
 
-def attention(inputs, attention_size, return_alphas=False):
+from tensorflow.keras.layers import Input, LSTM, Bidirectional, Dense, Lambda, BatchNormalization, Dropout, Layer
+from tensorflow.keras.models import Model
 
-    # Dense layers for computing attention scores
-    w_omega = Dense(attention_size, activation="tanh")
-    u_omega = Dense(1) 
+class AttentionLayer(Layer):
 
-    v = w_omega(inputs)  # Shape: (batch_size, time_steps, attention_size)
-    vu = u_omega(v)  # Shape: (batch_size, time_steps, 1)
+    def __init__(self, attention_size, return_alphas=False, **kwargs):
+        super(AttentionLayer, self).__init__(**kwargs)
+        self.attention_size = attention_size
+        self.return_alphas = return_alphas
 
-    alphas = tf.nn.softmax(vu, axis=1)
+    def build(self, input_shape):
+        # input_shape should be: (batch_size, time_steps, features)
+        self.w_omega = Dense(self.attention_size, activation="tanh", name="att_dense_tanh")
+        self.u_omega = Dense(1, name="att_dense_linear")
+        super(AttentionLayer, self).build(input_shape)
 
-    output = tf.reduce_sum(inputs * alphas, axis=1)  # Shape: (batch_size, features)
+    def call(self, inputs, **kwargs):
 
-    if return_alphas:
-        return output, alphas
-    return output
+        v = self.w_omega(inputs)  # Shape: (batch_size, time_steps, attention_size)
+        vu = self.u_omega(v)      # Shape: (batch_size, time_steps, 1)
+        
+        # Compute the attention weights by applying softmax on the time dimension
+        alphas = tf.nn.softmax(vu, axis=1)  # Shape: (batch_size, time_steps, 1)
+        
+        output = tf.reduce_sum(inputs * alphas, axis=1)  # Shape: (batch_size, features)
+        
+        if self.return_alphas:
+            return output, alphas
+        return output
+    
+    def compute_output_shape(self, input_shape):
+        output_shape = (input_shape[0], input_shape[-1])
+        if self.return_alphas:
+            alphas_shape = (input_shape[0], input_shape[1], 1)
+            return [output_shape, alphas_shape]
+        return output_shape
 
+    def get_config(self):
+        config = super(AttentionLayer, self).get_config()
+        config.update({
+            "attention_size": self.attention_size,
+            "return_alphas": self.return_alphas,
+        })
+        return config
 
-def BiLSTM_with_Attention(max_time, n_input, lstm_size, attention_size, keep_prob, weights_1, biases_1, weights_2, biases_2):
+def BiLSTM_with_Attention(max_time, n_input, lstm_size, attention_size, keep_prob, n_hidden, n_class):
 
     # Reshaping input eeg signal for BiLSTM
-    input_layer = Input(shape=(max_time, n_input))
+    input_layer = Input(shape=(n_input,))
+    reshaped_layer = Lambda(lambda x: tf.reshape(x, (-1, max_time, n_input)))(input_layer)
 
     # BiLSTM Model with dropout
     bilstm_layer = Bidirectional(
         LSTM(units=lstm_size, dropout=1 - keep_prob, return_sequences=True)
     )
 
-    outputs = bilstm_layer(input_layer)
+    outputs = bilstm_layer(reshaped_layer)
+
+    attention_output, alphas = AttentionLayer(attention_size, return_alphas=True)(outputs)
+
+    attention_output = Dropout(1 - keep_prob)(attention_output)
+
+    # Using Keras Dense layer with softplus activation.
+    fc1 = Dense(
+        n_hidden,
+        activation='softplus',
+        kernel_initializer=tf.keras.initializers.TruncatedNormal(stddev=0.01),
+        bias_initializer=tf.keras.initializers.Constant(0.01),
+        kernel_regularizer=tf.keras.regularizers.l2(0.001)
+    )(attention_output)
+    fc1 = BatchNormalization()(fc1)
+    fc1 = Dropout(1 - keep_prob)(fc1)
+
+    # Second fully-connected layer: output layer with softmax activation.
+    fc2 = Dense(
+        n_class,
+        activation='softmax',
+        kernel_initializer=tf.keras.initializers.TruncatedNormal(stddev=0.01),
+        bias_initializer=tf.keras.initializers.Constant(0.01)
+    )(fc1)
+
+    reshaped_output_layer = Lambda(lambda x: tf.repeat(x, repeats=64, axis=0))(fc2)
+
+    model = Model(inputs=input_layer, outputs=reshaped_output_layer)
+    return model
+
+
