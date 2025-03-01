86815eac739a9ec488aff677509d49626d4cbcbd
Deepak Singh <negid0253@gmail.com>
2025-03-01 12:57:35 +0530

    feat: Added time distributed layer

--- Models/Network/BiLSTM_with_Attention.py
+++ Models/Network/BiLSTM_with_Attention.py
@@ -1,10 +1,19 @@
 import tensorflow as tf
 
-from tensorflow.keras.layers import Input, LSTM, Bidirectional, Dense, Lambda, BatchNormalization, Dropout, Layer
+from tensorflow.keras.layers import Input, LSTM, Bidirectional, Dense, BatchNormalization, Dropout, TimeDistributed, Reshape
 from tensorflow.keras.models import Model
 
 from Network.Custom_Layers.ReshapeLayer import ReshapeLayer
 from Network.Custom_Layers.AttentionLayer import AttentionLayer
+
+@tf.custom_gradient
+def binary_activation_ste(x):
+    # Forward pass: threshold at 0.5 to produce binary outputs (0 or 1)
+    y = tf.where(x >= 0.5, tf.ones_like(x), tf.zeros_like(x))
+    def grad(dy):
+        # Backward pass: use identity gradient (or any suitable surrogate)
+        return dy
+    return y, grad
 
 def BiLSTM_with_Attention(max_time, n_input, lstm_size, attention_size, drop_out, n_hidden, n_class):
 
@@ -12,12 +21,21 @@ def BiLSTM_with_Attention(max_time, n_input, lstm_size, attention_size, drop_out
     input_layer = Input(shape=(max_time * n_input,))
     reshaped_layer = ReshapeLayer(max_time, n_input)(input_layer)
 
+    channel_layer = Reshape((max_time, n_input, 1))(reshaped_layer)
+
+    layer_1 = TimeDistributed(Dense(10, activation="sigmoid"))(channel_layer)
+
+    # Second TimeDistributed layer with 20 neurons and custom binary activation
+    mapped_layer = TimeDistributed(Dense(20, activation=binary_activation_ste))(layer_1)
+
+    flattened_layer = Reshape((max_time, n_input * 10))(mapped_layer)
+
     # BiLSTM Model with dropout
     bilstm_layer = Bidirectional(
         LSTM(units=lstm_size, dropout=drop_out, return_sequences=True)
     )
 
-    outputs = bilstm_layer(reshaped_layer)
+    outputs = bilstm_layer(flattened_layer)
 
     attention_output, alphas = AttentionLayer(attention_size, return_alphas=True)(outputs)
 
